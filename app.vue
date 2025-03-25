92c32d9c9d74e1411be2d3c3820e1265ebd038e2
ChrisDemey <49678590+ChrisDemey@users.noreply.github.com>
2025-03-25 17:27:47 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="h-screen w-full flex items-center bg-dark text-light justify-center"
+  >
+    <div class="w-full max-w-6xl aspect-video flex">
+      <!-- gauche -->
+      <section class="w-1/2 pr-4">
+        <div class="flex size-full items-center justify-center">
+          <div>
+            <h1>chriswave</h1>
+            <h2>developpeur web</h2>
+            <h3>
+              mon travail est d'aider les gens en leur créant des sites web &
+              des solutions digitales
+            </h3>
+          </div>
+          <img src="" alt="" />
+        </div>
+      </section>
+      <!-- droite -->
+      <section class="w-1/2 h-full">
+        <div class="grid size-full grid-cols-4 grid-rows-6 gap-4">
+          <grid-card class="col-span-2 row-span-2">
+            <p>portfolio</p>
+          </grid-card>
+          <grid-card class="col-span-2 row-span-3">
+            <p>discutons de votre projet</p>
+          </grid-card>
+          <grid-card class="col-span-2 row-span-3">
+            <p>pourquoi faire appel à un dev ?</p>
+          </grid-card>
+          <grid-card class="col-span-2 row-span-3">
+            <p>socials</p>
+          </grid-card>
+          <grid-card class="col-span-2 row-span-1">
+            <p>switchers</p>
+          </grid-card>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
---
+++ components/GridCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="size-full p-6 bg-card-dark rounded-xl border border-medium-gray">
+    <slot />
+  </div>
+</template>

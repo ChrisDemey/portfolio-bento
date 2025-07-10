808f150c73462e63bae420cb80788717fc512e3f
ChrisDemey <49678590+ChrisDemey@users.noreply.github.com>
2025-07-10 12:49:46 +0200

    Added bento box model card in models modal

--- app.vue
+++ app.vue
@@ -3,7 +3,9 @@
     class="relative flex justify-center items-center bg-dark w-full min-h-screen font-montserrat text-light-gray"
   >
     <div class="z-0 absolute inset-0 opacity-70 background--custom" />
-    <div class="z-5 absolute inset-0 bg-radial from-dark via-dark/70 to-transparent" />
+    <div
+      class="z-5 absolute inset-0 bg-radial from-dark via-dark/70 to-transparent"
+    />
     <img
       src="/assets/Chriswave_logo.png"
       alt="chriswave_logo"
@@ -16,28 +18,27 @@
       <section
         class="flex justify-center items-center w-full lg:max-w-1/2 lg:h-full"
       >
-        <div
-          class="relative flex flex-col justify-start items-center"
-        >
+        <div class="relative flex flex-col justify-start items-center">
           <div class="z-10 relative w-full uppercase">
             <h1
               class="font-thin text-3xl lg:text-4xl text-center lg:text-end tracking-widest"
             >
               développeur web
             </h1>
-            <h2
-              class="font-bold text-4xl lg:text-8xl text-center lg:text-end"
-            >
+            <h2 class="font-bold text-4xl lg:text-8xl text-center lg:text-end">
               Chriswave
             </h2>
             <h3
               class="mt-4 lg:pr-2 font-thin text-xl lg:text-3xl text-center lg:text-end leading-10 tracking-widest"
             >
-              Création de <span class="font-bold">sites web</span> et <span class="font-bold">solutions digitales</span> (SaaS)
+              Création de <span class="font-bold">sites web</span> et
+              <span class="font-bold">solutions digitales</span> (SaaS)
             </h3>
           </div>
           <!-- Flèche animée pour mobile -->
-          <div class="lg:hidden right-0 bottom-0 left-0 absolute flex justify-center mt-12 -mb-20">
+          <div
+            class="lg:hidden right-0 bottom-0 left-0 absolute flex justify-center mt-12 -mb-20"
+          >
             <svg
               class="border-2 rounded-full w-8 h-8 text-current animate-bounce cursor-pointer"
               xmlns="http://www.w3.org/2000/svg"
@@ -65,41 +66,53 @@
         <!-- Modal for Portfolio -->
         <Modal :is-open="portoflioModal" @close="portoflioModal = false">
           <div class="gap-8 lg:gap-4 grid lg:grid-cols-4">
-            <div v-for="company in projectsByCompany" :key="company.company" class="col-span-1 lg:col-span-4">
-                <h1
-                  class="mb-2 pl-5 font-thin text-3xl uppercase tracking-widest"
+            <div
+              v-for="company in projectsByCompany"
+              :key="company.company"
+              class="col-span-1 lg:col-span-4"
+            >
+              <h1
+                class="mb-2 pl-5 font-thin text-3xl uppercase tracking-widest"
+              >
+                {{ company.company }}
+              </h1>
+              <div
+                v-for="project in company.projects"
+                :key="project.name"
+                class="flex lg:flex-row flex-col lg:space-x-6 bg-light/10 backdrop-blur-2xl mb-4 p-3 border border-medium-gray rounded-xl h-auto lg:h-48 overflow-hidden"
+              >
+                <a
+                  v-if="project.link"
+                  :href="project.link"
+                  target="_blank"
+                  class="rounded-lg w-full lg:w-1/2 aspect-video overflow-hidden scale-100 hover:scale-102 transition"
                 >
-                  {{ company.company }}
-                </h1>
+                  <img
+                    :src="project.image"
+                    :alt="project.name"
+                    class="size-full object-cover"
+                  />
+                </a>
                 <div
-                  v-for="project in company.projects"
-                  :key="project.name"
-                  class="flex lg:flex-row flex-col lg:space-x-6 bg-card-dark mb-4 p-3 border border-medium-gray rounded-xl h-auto lg:h-48 overflow-hidden"
+                  v-else
+                  class="rounded-lg w-full lg:w-1/2 aspect-video overflow-hidden"
                 >
-                  <a v-if="project.link" :href="project.link" target="_blank" class="w-full lg:w-1/2 aspect-video overflow-hidden scale-100 hover:scale-102 transition">
-                    <img
-                      :src="project.image"
-                      :alt="project.name"
-                      class="rounded-lg size-full object-cover"
-                    />
-                  </a>
-                  <div v-else class="w-full lg:w-1/2 aspect-video overflow-hidden">
-                    <img
-                      :src="project.image"
-                      :alt="project.name"
-                      class="rounded-lg size-full object-cover"
-                    />
-                  </div>
-                  <div
-                    class="flex flex-col justify-start mt-4 lg:mt-0 pr-2 w-full lg:w-1/2 overflow-y-auto"
-                  >
-                    <h3 class="font-medium text-xl uppercase tracking-wider">
-                      {{ project.name }}
-                    </h3>
-                    <p class="font-light text-justify tracking-wide">
-                      {{ project.description }}
-                    </p>
-                  </div>
+                  <img
+                    :src="project.image"
+                    :alt="project.name"
+                    class="size-full object-cover"
+                  />
+                </div>
+                <div
+                  class="flex flex-col justify-start mt-4 lg:mt-0 pr-2 w-full lg:w-1/2 overflow-y-auto"
+                >
+                  <h3 class="font-medium text-xl uppercase tracking-wider">
+                    {{ project.name }}
+                  </h3>
+                  <p class="font-light text-justify tracking-wide">
+                    {{ project.description }}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -107,47 +120,68 @@
         <!-- Modal for models -->
         <Modal :is-open="modelsModal" @close="modelsModal = false">
           <div
-              class="col-span-1 lg:col-span-4 bg-light-gray mt-4 px-3 pt-6 pb-3 rounded-2xl"
+            class="col-span-1 lg:col-span-4 bg-light-gray/80 backdrop-blur-lg mt-4 px-3 pt-6 pb-3 rounded-2xl"
+          >
+            <h2
+              class="mb-6 font-bold text-dark text-2xl lg:text-3xl text-center uppercase tracking-wider"
             >
-              <h2
-                class="mb-6 font-bold text-dark text-2xl lg:text-3xl text-center uppercase tracking-wider"
+              Modèles de base
+            </h2>
+            <div class="gap-3 grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2">
+              <a
+                v-for="model in websitesModels"
+                :key="model.name"
+                :href="model.link"
+                target="_blank"
+                class="group relative aspect-video"
               >
-                Modèles de base
-              </h2>
-              <div class="gap-6 grid grid-cols-1 lg:grid-cols-2">
-                <a
-                  v-for="model in websitesModels"
-                  :key="model.name"
-                  :href="model.link"
-                  target="_blank"
-                  class="group relative aspect-video"
+                <div
+                  class="z-0 absolute bg-gradient-to-b from-[#F472B6] to-[#5EEAD4] to-75% shadow-teal-500/50 group-hover:shadow-lg rounded-xl size-full"
+                />
+                <div
+                  class="z-10 absolute group-hover:p-1.5 size-full duration-300 transform"
                 >
                   <div
-                    class="z-0 absolute bg-gradient-to-b from-[#F472B6] to-[#5EEAD4] to-75% shadow-teal-500/50 group-hover:shadow-lg rounded-xl size-full"
-                  />
-                  <div
-                    class="z-10 absolute group-hover:p-1.5 size-full duration-300 transform"
+                    class="relative flex justify-center items-center bg-card-dark rounded-xl group-hover:rounded-lg size-full overflow-hidden text-light-gray duration-300 transform"
                   >
-                    <div
-                      class="relative flex justify-center items-center bg-card-dark border border-medium-gray group-hover:border-0 rounded-xl group-hover:rounded-lg size-full overflow-hidden text-light-gray duration-300 transform"
-                    >
-                      <div class="z-10 text-center">
-                        <h3 class="font-bold text-3xl uppercase tracking-wider">
-                          {{ model.name }}
-                        </h3>
-                        <p class="tracking-widest">
-                          {{ model.description }}
-                        </p>
-                      </div>
-                      <component
-                        class="-right-2 -bottom-10 z-0 absolute"
-                        :is="model.icon"
-                      />
+                    <div class="z-10 text-center">
+                      <h3 class="font-bold text-3xl uppercase tracking-wider">
+                        {{ model.name }}
+                      </h3>
+                      <p class="tracking-widest">
+                        {{ model.description }}
+                      </p>
                     </div>
+                    <component
+                      class="-right-2 -bottom-10 z-0 absolute"
+                      :is="model.icon"
+                    />
                   </div>
-                </a>
+                </div>
+              </a>
+              <div
+                class="relative col-span-2 bg-card-dark hover:opacity-20 rounded-xl size-full overflow-hidden text-light-gray transition cursor-not-allowed"
+              >
+                <div
+                  class="z-10 relative flex flex-col justify-center items-center pl-4 w-2/3 h-full"
+                >
+                  <h3 class="font-bold text-3xl uppercase tracking-wider">
+                    Bento box
+                  </h3>
+                  <p class="tracking-widest">Plus moderne, plus personnel</p>
+                </div>
+                <div
+                  class="right-0 bottom-0 z-0 absolute flex justify-end items-center size-full"
+                >
+                  <Icon
+                    class="text-medium-gray"
+                    size="220"
+                    name="ic:round-bento"
+                  />
+                </div>
               </div>
             </div>
+          </div>
         </Modal>
         <!-- Portoflio -->
         <grid-card
@@ -190,7 +224,8 @@
         >
           <div
             class="bottom-4 z-10 absolute w-[80%] font-thin text-xl lg:text-2xl uppercase duration-300 transform"
-          > Mes
+          >
+            Mes
             <span
               class="after:block after:bottom-0 after:left-0 after:absolute relative after:bg-current after:w-0 group-hover:after:w-full after:h-px after:transition-width after:duration-300"
               >modèles</span
@@ -321,10 +356,10 @@ const projectsByCompany = [
         image: "/assets/nirvana_screenshot.png",
         description:
           "J'ai réalisé ce site vitrine pour un centre de beauté, il s'agissait de concevoir une page d'accueil simple et efficace pour présenter leurs services.",
-        link: "https://www.nirvanacentredebienetre.com/"
-      }
-    ]
-  }
+        link: "https://www.nirvanacentredebienetre.com/",
+      },
+    ],
+  },
 ];
 
 const websitesModels = [
